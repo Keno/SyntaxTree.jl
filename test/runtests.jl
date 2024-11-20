dd690738cada51370cf3a2c16bd14c1d8b1a6c3a
Keno Fischer <keno@juliahub.com>
2024-11-20 04:49:41 +0000

    Add explicit `invokelatest`
    These will be required in Julia 1.12 (as they would be in local scope).
    See https://github.com/JuliaLang/julia/pull/56509.

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,9 +15,9 @@ else
 @test SyntaxTree.abs(:((x-1)^2+$(Expr(:macrocall,Symbol("@big_str"),nothing,"2")))) == :((x + 1) ^ 2 + $(Expr(:macrocall,Symbol("@big_str"),nothing,"2")))
 end
 @test SyntaxTree.alg(:(x+1)) == :((1 + ϵ) * (x + 1))
-@test (f = SyntaxTree.genfun(:x,:x); f(1) == 1)
-@test (f = SyntaxTree.genfun(:x,(:x,:y)); f(1,0) == 1)
-@test (f = SyntaxTree.genlatest(:x,:x); f(1) == 1)
-@test (f = SyntaxTree.genlatest(:x,(:x,:y)); f(1,0) == 1)
+@test (f = SyntaxTree.genfun(:x,:x); invokelatest(f, 1) == 1)
+@test (f = SyntaxTree.genfun(:x,(:x,:y)); invokelatest(f,1,0) == 1)
+@test (f = SyntaxTree.genlatest(:x,:x); invokelatest(f,1) == 1)
+@test (f = SyntaxTree.genlatest(:x,(:x,:y)); invokelatest(f,1,0) == 1)
 @test callcount(:(x+y*z)) == 2
 @test SyntaxTree.exprval(:(x^2-2))[1] == 0.0
